4a7c0ebd3522a0390f0ca3f4fa2a103425f727b2
Gary Strong <gary.m.strong@gmail.com>
2012-06-23 19:49:47 +0100

    Adding Input Handler from tutorials

---
+++ GameEngine/Input/InputHandler.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameEngine.Input
+{
+    public class InputHandler : GameComponent
+    {
+        static KeyboardState keyboardState;
+        static KeyboardState lastKeyboardState;
+
+        static GamePadState[] gamePadStates;
+        static GamePadState[] lastGamePadStates;
+
+        public static KeyboardState KeyboardState
+        {
+            get { return keyboardState; }
+        }
+
+        public static KeyboardState LastKeyboardState
+        {
+            get { return lastKeyboardState; }
+        }
+
+        public static GamePadState[] GamePadStates
+        {
+            get { return gamePadStates; }
+        }
+
+        public static GamePadState[] LastGamePadStates
+        {
+            get { return lastGamePadStates; }
+        }
+
+        public InputHandler(Game game)
+            : base(game)
+        {
+            keyboardState = Keyboard.GetState();
+
+            gamePadStates = new GamePadState[Enum.GetValues(typeof(PlayerIndex)).Length];
+
+            foreach (PlayerIndex index in Enum.GetValues(typeof(PlayerIndex)))
+                gamePadStates[(int)index] = GamePad.GetState(index);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            lastKeyboardState = keyboardState;
+            keyboardState = Keyboard.GetState();
+
+            lastGamePadStates = (GamePadState[])gamePadStates.Clone();
+            foreach (PlayerIndex index in Enum.GetValues(typeof(PlayerIndex)))
+                gamePadStates[(int)index] = GamePad.GetState(index);
+
+            base.Update(gameTime);
+        }
+
+        public static void Flush()
+        {
+            lastKeyboardState = keyboardState;
+        }
+
+        public static bool KeyReleased(Keys key)
+        {
+            return keyboardState.IsKeyUp(key) &&
+                   lastKeyboardState.IsKeyDown(key);
+        }
+
+        public static bool KeyPressed(Keys key)
+        {
+            return keyboardState.IsKeyDown(key) &&
+                   lastKeyboardState.IsKeyUp(key);
+        }
+
+        public static bool KeyDown(Keys key)
+        {
+            return keyboardState.IsKeyDown(key);
+        }
+
+        public static bool ButtonReleased(Buttons button, PlayerIndex index)
+        {
+            return gamePadStates[(int)index].IsButtonUp(button) &&
+                   lastGamePadStates[(int)index].IsButtonDown(button);
+        }
+
+        public static bool ButtonPressed(Buttons button, PlayerIndex index)
+        {
+            return gamePadStates[(int)index].IsButtonDown(button) &&
+                   lastGamePadStates[(int)index].IsButtonUp(button);
+        }
+
+        public static bool ButtonDown(Buttons button, PlayerIndex index)
+        {
+            return gamePadStates[(int)index].IsButtonDown(button);
+        }
+    }
+}
