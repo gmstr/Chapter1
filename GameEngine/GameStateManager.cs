59dbc7e7b52efe9e10643633a09eb2db30db94e6
Gary Strong <gary.m.strong@gmail.com>
2012-06-23 16:06:51 +0100

    Adding game engine dll and game state classes from tutorials

---
+++ GameEngine/GameStateManager.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace GameEngine
+{
+    public class GameStateManager : GameComponent
+    {
+        public event EventHandler OnStateChange;
+
+        Stack<GameState> gameStates = new Stack<GameState>();
+
+        const int startDrawOrder = 5000;
+        const int drawOrderInc = 100;
+        int drawOrder;
+
+        public GameState CurrentState
+        {
+            get { return gameStates.Peek(); }
+        }
+
+        public GameStateManager(Game game)
+            : base(game)
+        {
+            drawOrder = startDrawOrder;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public void PopState()
+        {
+            if (gameStates.Count > 0)
+            {
+                RemoveState();
+                drawOrder -= drawOrderInc;
+
+                if (OnStateChange != null)
+                    OnStateChange(this, null);
+            }
+        }
+
+        private void RemoveState()
+        {
+            GameState State = gameStates.Peek();
+
+            OnStateChange -= State.StateChange;
+            Game.Components.Remove(State);
+            gameStates.Pop();
+        }
+
+        public void PushState(GameState newState)
+        {
+            drawOrder += drawOrderInc;
+            newState.DrawOrder = drawOrder;
+
+            AddState(newState);
+
+            if (OnStateChange != null)
+                OnStateChange(this, null);
+        }
+
+        private void AddState(GameState newState)
+        {
+            gameStates.Push(newState);
+
+            Game.Components.Add(newState);
+
+            OnStateChange += newState.StateChange;
+        }
+
+        public void ChangeState(GameState newState)
+        {
+            while (gameStates.Count > 0)
+                RemoveState();
+
+            newState.DrawOrder = startDrawOrder;
+            drawOrder = startDrawOrder;
+
+            AddState(newState);
+
+            if (OnStateChange != null)
+                OnStateChange(this, null);
+        }
+    }
+}
---
+++ GameEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GameEngine")]
+[assembly: AssemblyProduct("GameEngine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("faf8325c-9f59-46a3-b037-cc6bb053b6d2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
