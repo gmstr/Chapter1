573734cbe403e9f468a43c6c808c7fb43fc8c707
Gary Strong <gary.m.strong@gmail.com>
2012-06-30 20:23:04 +0100

    Adding an IsNaviagatable flag, and moving classes into own files

--- GameEngine/Map/AreaMap.cs
+++ GameEngine/Map/AreaMap.cs
@@ -1,37 +1,13 @@
-﻿namespace GameEngine.Map
+﻿using System.Collections.Generic;
+
+namespace GameEngine.Map
 {
     public class AreaMap
     {
         public int Width { get; set; }
         public int Height { get; set; }
 
-        public MapTile[,] FloorTiles { get; set; }
-
-        public MapObject[] MapObjects { get; set; }
-    }
-
-    public class MapObject
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public int Width { get; set; }
-        public int Height { get; set; }
-
-        public MapOrientation Orientation { get; set; }
-    }
-
-    public enum MapOrientation
-    {
-        North,
-        East,
-        South,
-        West
-    }
-
-    public class MapTile
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public int Z { get; set; }
+        public List<MapTile> FloorTiles { get; set; }
+        public List<MapObject> MapObjects { get; set; }
     }
 }
