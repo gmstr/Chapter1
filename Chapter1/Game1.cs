1af478a594b7fefa41d80e57c9e266619d728d96
Gary Strong <gary.m.strong@gmail.com>
2012-06-30 20:29:56 +0100

    Updating background colour

--- Chapter1/Game1.cs
+++ Chapter1/Game1.cs
@@ -81,7 +81,7 @@ namespace Chapter1
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Magenta);
 
             // TODO: Add your drawing code here
 
