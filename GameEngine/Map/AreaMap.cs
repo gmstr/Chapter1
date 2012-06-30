f561a5903c6ca18693adca4de24bbbf06aad0988
Gary Strong <gary.m.strong@gmail.com>
2012-06-30 19:46:15 +0100

    Adding in basics for map

---
+++ GameEngine/Map/AreaMap.cs
@@ -0,0 +1,37 @@
+﻿namespace GameEngine.Map
+{
+    public class AreaMap
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public MapTile[,] FloorTiles { get; set; }
+
+        public MapObject[] MapObjects { get; set; }
+    }
+
+    public class MapObject
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public MapOrientation Orientation { get; set; }
+    }
+
+    public enum MapOrientation
+    {
+        North,
+        East,
+        South,
+        West
+    }
+
+    public class MapTile
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+    }
+}
